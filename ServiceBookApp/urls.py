b9188a4f0b3ab26f7ec580d77af2bcd169d03563
Joaquin131512 <jo.mendezo@duocuc.cl>
2025-11-28 09:59:44 -0300

    Project of BookShop

---
+++ ServiceBookApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.service, name='service'),#Quienes somos que hacemos
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
