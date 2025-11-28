3c48c2a85002a254b246da1fc8ca6db94b991568
Joaquin <128087107+Joaquin131512@users.noreply.github.com>
2025-11-28 09:57:24 -0300

    Delete BookShop directory

--- BookShop/ServiceBookApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServicebookappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ServiceBookApp'
--- BookShop/ServiceBookApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from ServiceBookApp.models import Service
-
-# Create your views here.
-def service(request):
-    service = Service.objects.all()
-    return render(request, 'Service/service.html', {'services': service})
